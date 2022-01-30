d0bb035aebce9b302fb0bb6b074eb0be2afdc8f7
pattarapon044 <6431503044@lamduan.mfu.ac.th>
2022-01-30 15:52:34 +0700

    add Homework version

--- src/exercise2/Blackjack.java
+++ src/exercise2/Blackjack.java
@@ -1,229 +1,165 @@
 package exercise2;
 
 import java.util.Random;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Blackjack{
+public class Blackjack {
 	
-	private static Scanner input = new Scanner(System.in);
+	//Create Attribute
+	private int[] cardYou;
+	private int[] cardComputer;
+	private int numYourCard;
+	private int sumYou;
+	private int sumComputer;
+	private String winner;
 	
-	//Attribute
-    private int[] playerCard;
-    private int[] computerCard;
-    
-    private int playerSum;
-    private int computerSum;
-    
-    private int numPlayerCard;
-    
-    private String winner;
-    private boolean end;
-	
-	//Private : draw
-	private int drawCard() {
-		Random r = new Random();
-		return r.nextInt(11)+1;
+	//Method : card
+	private int[] drawCard(int n) {
+		//Prepare array to stored card
+		int[] card = new int[n];
+		
+		//Add random card to array
+		for (int i=0; i<n; i++) {
+			card[i] = new Random().nextInt(11)+1;
+		}
+		
+		//Return card after random it
+		return card;
 	}
 	
-    //Constructor
-    public Blackjack(){
-    	//Setup : data
-		playerSum = 0;
-        computerSum = 0;
-        end = false;
-        
-        //Setup : computer
-        computerCard = new int[] {drawCard(), drawCard()};
-        computerSum = getComputerSum();
-        if (computerSum == 11+11) {
-        	computerCard[1] = 1;
-        }
-        computerSum = getComputerSum();
-        
-        //Setup : player
-        playerCard = new int[] {drawCard(), drawCard(), 0, 0, 0};
-        if (getPlayerSum() == 11+11) {
-        	playerCard[1] = 1;
-        }
-        playerSum = getPlayerSum();
-        numPlayerCard = 2;
-    }
-    
-    //Method : player
-    public void showPlayerCard(){
-    	String card = "";
-    	for (int i = 0; i<playerCard.length; i++) {
-    		card += playerCard[i]+" ";
-    	}
-        System.out.println("Player   : "+card);
-    }
-    
-    //Method : computer
-    public void showComputerCard(){
-    	//Do not show computer card until game end
-    	if (isEnd()) {
-    		//Prepared
-	    	String card = new String("");
-	    	
-	    	//Format output
-	    	for (int i = 0; i<computerCard.length; i++) {
-	    		card += computerCard[i]+" ";
-	    	}
-	    	//Out put
-	    	System.out.println("Computer : "+card);
-    	}
-    	else {
-    		System.out.println("Computer : ? ?");
-    	}
-    }
-    
-    //Method : sum card
-    public void showSumCard(){
-    	System.out.println("Sum of Player card    =  "+ getPlayerSum());
-    	System.out.println("Sum of Computer card  =  "+ getComputerSum());
-    }
-    
-    //Method : player
-    public int getPlayerSum() {
-    	playerSum = 0;
-    	for (int card:playerCard) {
-    		playerSum += card;
-    	}
-    	return playerSum;
-    }
-    
-    //Method : computer
-    public int getComputerSum() {
-    	computerSum = 0;
-    	for (int card:computerCard) {
-    		computerSum += card;
-    	}
-    	return computerSum;
-    }
-    
-    //Method : winner
-    public String getWinner(){
-    	//Player : Bust
-    	if (getPlayerSum() > 21) {
-    		winner = "Computer!!";
-    	}
-    	//Player : win
-    	else if(getPlayerSum() > getComputerSum()){
-    		
-    		if (getPlayerSum() == 21) {
-    			winner = "Player Blackjack!!";
-    		}
-    		else {
-    			winner = "Player!!";
-    		}
-    	}
-    	//Player : Lose
-        else {
-        	if (getComputerSum() == 21) {
-        		winner = "Computer Blackjack!!";
-        	}
-        	else {
-        		winner = "Computer!!";
-        	}
-        }
-    	
-        return winner;
-    }
-    
-    //Method : player
-    public void addMoreCard() {
-    	
-    	//Check if can draw
-    	if (numPlayerCard < 5) {
-    		
-    		//If draw A card
-    		int draw = drawCard();
-    		if (draw == 11 && getPlayerSum()+draw > 21) {
-    			draw = 1;
-    		}
-    		
-    		//Add card
-    		playerCard[numPlayerCard] = draw;
-    		numPlayerCard += 1;
-    	}
-    }
-    
-    //Method : check end
-    public boolean isEnd() {
-    	if (end) {
-    		return end;
-    	}
-    	else if (getPlayerSum() >= 21) {
-    		return true;
-    	}
-    	else if (numPlayerCard == 5) {
-    		return true;
-    	}
-    	
-    	return false;
-    }
-    
-    //Process : start
-    public void start(){
-        //Loop
-    	do {
-	        //Show card
-	        showPlayerCard();
-	        showComputerCard();
-	        System.out.println("Player sum : "+getPlayerSum());
-	        System.out.println();
-	        
-	        //Ask for add card
-	        System.out.print("Want another card? <y /n> : ");
-	        String check = input.nextLine();
-	        
-	        if (check.equals("y")) {
-	        	addMoreCard();
-	        }
-	        else {
-	        	end = true;
-	        	break;
-	        }
-	       
-	        System.out.println();
-	        
-    	} while(!isEnd());
-        
-        //Out come result
-    	System.out.println();
-        getGameResult();
-    }
-    
-    //Process : end
-    public void getGameResult() {
-    	//Output
-    	showPlayerCard();
-    	showComputerCard();
-    	System.out.println();
-    	showSumCard();
-    	System.out.println("The winner is : "+ getWinner());
-    }
-    
-    //Main : Check here
-    public static void main(String[] args) {
-    	String loop = "y";
-    	
-    	//Loop
-    	do {
-    		
-    		//Start game
-    		Blackjack bj = new Blackjack();
-    		bj.start();
-    		
-    		//Check for next more game
-    		System.out.print("\nNext more game? <y/n> : ");
-    		loop = input.nextLine();
-    		
-    	} while (loop.equals("y"));
-    	
-    	//End program
-    	System.out.println("Good Bye!!");
-    }
+	//Class : constructor
+	public Blackjack() {
+		
+		//Initialize class Attribute
+		cardYou = drawCard(5);
+		cardComputer = drawCard(2);
+		numYourCard = 2;
+		sumYou = cardYou[0]+cardYou[1];
+		sumComputer = cardComputer[0]+cardComputer[1];
+		winner = null;
+		
+		//Check is card both 11
+		if (sumYou == 22) {
+			cardYou[1] = 1;
+			sumYou = 12;
+		}
+		if (sumComputer == 22) {
+			cardComputer[1] = 1;
+			sumComputer = 12;
+		}
+	}
+	
+	//Method : you
+	public void showYouCard() {
+		String card = "You : ";
+		
+		//Check number card to Show
+		for (int i=0; i<5; i++) {
+			
+			if (i >= numYourCard) {
+				card += "0 ";
+			}
+			else
+			{
+				card += cardYou[i] + " ";
+			}
+		}
+		//Output
+		System.out.println(card);
+	}
+	
+	//Method : computer
+	public void showComputerCard() {
+		System.out.print("Computer : ");
+		System.out.print(cardComputer[0] + " ");
+		System.out.println(cardComputer[1]);
+	}
+	
+	//Method : card, you
+	public void addMoreCard() {
+		//Check card is 5?
+		if (numYourCard < 5) {
+			
+			//If draw is 11 and more than 21
+			if (cardYou[numYourCard] == 11 && (sumYou + cardYou[numYourCard]) > 21) {
+				cardYou[numYourCard] = 1;
+			}
+			
+			//update 
+			sumYou += cardYou[numYourCard];
+			numYourCard ++;
+		}
+	}
+	
+	//Method : card
+	public void showSumCard() {
+		System.out.println("Sum of Your card = " + sumYou);
+		System.out.println("Sum of Computer card = " + sumComputer);
+	}
+	
+	//Method : game
+	public boolean isEnd() {
+		//First check
+		if (sumYou >= 21 || numYourCard == 5) {
+			return true;
+		}
+		
+		//Ask for more card
+		System.out.print("Want another card ? (y/n)...");
+		@SuppressWarnings("resource")
+		String check = new Scanner(System.in).nextLine();
+		
+		//Check
+		if (check.equals("y")) {
+			addMoreCard();
+			
+			if (sumYou >= 21 || numYourCard == 5) {
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	//Method : game
+	public void checkWinner() {
+		if (sumYou > 21) {
+			winner = "Computer";
+		}
+		else if (sumYou > sumComputer) {
+			winner = "You";
+		}
+		else {
+			winner = "Computer";
+		}
+	}
+	
+	//Method : game
+	public String getWinner() {
+		return winner;
+	}
+	
+	//Method : main
+	public static void main(String[] args) {
+		//start game
+		Blackjack bj = new Blackjack();
+		
+		do {
+			//Main output
+			bj.showYouCard();
+			System.out.println("Computer : ? ?\n");
+			
+		} while (!(bj.isEnd()));
+		
+		//Print out game result
+		bj.showYouCard();
+		bj.showComputerCard();
+		System.out.println();
+		bj.showSumCard();
+		bj.checkWinner();
+		System.out.println("The winner is " + bj.getWinner());
+	}
+	
 }
-
---
+++ src/exercise2/MyBlackjack.java
@@ -0,0 +1,228 @@
+package exercise2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyBlackjack{
+	
+	private static Scanner input = new Scanner(System.in);
+	
+	//Attribute
+    private int[] playerCard;
+    private int[] computerCard;
+    
+    private int playerSum;
+    private int computerSum;
+    
+    private int numPlayerCard;
+    
+    private String winner;
+    private boolean end;
+	
+	//Private : draw
+	private int drawCard() {
+		Random r = new Random();
+		return r.nextInt(11)+1;
+	}
+	
+    //Constructor
+    public MyBlackjack(){
+    	//Setup : data
+		playerSum = 0;
+        computerSum = 0;
+        end = false;
+        
+        //Setup : computer
+        computerCard = new int[] {drawCard(), drawCard()};
+        computerSum = getComputerSum();
+        if (computerSum == 11+11) {
+        	computerCard[1] = 1;
+        }
+        computerSum = getComputerSum();
+        
+        //Setup : player
+        playerCard = new int[] {drawCard(), drawCard(), 0, 0, 0};
+        if (getPlayerSum() == 11+11) {
+        	playerCard[1] = 1;
+        }
+        playerSum = getPlayerSum();
+        numPlayerCard = 2;
+    }
+    
+    //Method : player
+    public void showPlayerCard(){
+    	String card = "";
+    	for (int i = 0; i<playerCard.length; i++) {
+    		card += playerCard[i]+" ";
+    	}
+        System.out.println("Player   : "+card);
+    }
+    
+    //Method : computer
+    public void showComputerCard(){
+    	//Do not show computer card until game end
+    	if (isEnd()) {
+    		//Prepared
+	    	String card = new String("");
+	    	
+	    	//Format output
+	    	for (int i = 0; i<computerCard.length; i++) {
+	    		card += computerCard[i]+" ";
+	    	}
+	    	//Out put
+	    	System.out.println("Computer : "+card);
+    	}
+    	else {
+    		System.out.println("Computer : ? ?");
+    	}
+    }
+    
+    //Method : sum card
+    public void showSumCard(){
+    	System.out.println("Sum of Player card    =  "+ getPlayerSum());
+    	System.out.println("Sum of Computer card  =  "+ getComputerSum());
+    }
+    
+    //Method : player
+    public int getPlayerSum() {
+    	playerSum = 0;
+    	for (int card:playerCard) {
+    		playerSum += card;
+    	}
+    	return playerSum;
+    }
+    
+    //Method : computer
+    public int getComputerSum() {
+    	computerSum = 0;
+    	for (int card:computerCard) {
+    		computerSum += card;
+    	}
+    	return computerSum;
+    }
+    
+    //Method : winner
+    public String getWinner(){
+    	//Player : Bust
+    	if (getPlayerSum() > 21) {
+    		winner = "Computer!!";
+    	}
+    	//Player : win
+    	else if(getPlayerSum() > getComputerSum()){
+    		
+    		if (getPlayerSum() == 21) {
+    			winner = "Player Blackjack!!";
+    		}
+    		else {
+    			winner = "Player!!";
+    		}
+    	}
+    	//Player : Lose
+        else {
+        	if (getComputerSum() == 21) {
+        		winner = "Computer Blackjack!!";
+        	}
+        	else {
+        		winner = "Computer!!";
+        	}
+        }
+    	
+        return winner;
+    }
+    
+    //Method : player
+    public void addMoreCard() {
+    	
+    	//Check if can draw
+    	if (numPlayerCard < 5) {
+    		
+    		//If draw A card
+    		int draw = drawCard();
+    		if (draw == 11 && getPlayerSum()+draw > 21) {
+    			draw = 1;
+    		}
+    		
+    		//Add card
+    		playerCard[numPlayerCard] = draw;
+    		numPlayerCard += 1;
+    	}
+    }
+    
+    //Method : check end
+    public boolean isEnd() {
+    	if (end) {
+    		return end;
+    	}
+    	else if (getPlayerSum() >= 21) {
+    		return true;
+    	}
+    	else if (numPlayerCard == 5) {
+    		return true;
+    	}
+    	
+    	return false;
+    }
+    
+    //Process : start
+    public void start(){
+        //Loop
+    	do {
+	        //Show card
+	        showPlayerCard();
+	        showComputerCard();
+	        System.out.println("Player sum : "+getPlayerSum());
+	        System.out.println();
+	        
+	        //Ask for add card
+	        System.out.print("Want another card? <y /n> : ");
+	        String check = input.nextLine();
+	        
+	        if (check.equals("y")) {
+	        	addMoreCard();
+	        }
+	        else {
+	        	end = true;
+	        	break;
+	        }
+	       
+	        System.out.println();
+	        
+    	} while(!isEnd());
+        
+        //Out come result
+    	System.out.println();
+        getGameResult();
+    }
+    
+    //Process : end
+    public void getGameResult() {
+    	//Output
+    	showPlayerCard();
+    	showComputerCard();
+    	System.out.println();
+    	showSumCard();
+    	System.out.println("The winner is : "+ getWinner());
+    }
+    
+    //Main : Check here
+    public static void main(String[] args) {
+    	String loop = "y";
+    	
+    	//Loop
+    	do {
+    		
+    		//Start game
+    		MyBlackjack bj = new MyBlackjack();
+    		bj.start();
+    		
+    		//Check for next more game
+    		System.out.print("\nNext more game? <y/n> : ");
+    		loop = input.nextLine();
+    		
+    	} while (loop.equals("y"));
+    	
+    	//End program
+    	System.out.println("Good Bye!!");
+    }
+}
+
